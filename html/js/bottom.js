48a14932d379350fa6f306103dd1da8061b12604
jimmychou <koudaix700@gmail.com>
2013-09-04 15:34:02 +0800

    ok

---
+++ html/js/bottom.js
@@ -0,0 +1,3 @@
+document.write('<div class="footmune">');
+document.write('<span onclick=\"myAndroidJs.goToMenuPage(1);\"><img src=\"./img/home_web.png\"><p>首页</p></span><span onclick=\"window.myAndroidJs.goToMenuPage(2);\"><img src=\"./img/category_web.png\"><p>分类</p></span><span   onclick=\"window.myAndroidJs.goToMenuPage(3);\"><img src=\"./img/hot_web.png\"><p >特卖</p></span><span onclick=\"window.myAndroidJs.goToMenuPage(4);\"><img src=\"./img/hudong_web.png\"><p >互动</p></span><span  onclick=\"window.myAndroidJs.goToMenuPage(5);\"><img  style=\"width:42px;margin-top: 0px;\" src=\"./img/more_web.png\"><p >更多</p></span>');
+document.write('<div>');
