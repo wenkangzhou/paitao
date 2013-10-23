bfbd99d0c9ea7e76c1e5c59d2e4b3ea75f9c84ea
jimmychou <koudaix700@gmail.com>
2013-10-23 17:22:02 +0800

    commit

--- html/js/bottom.js
+++
@@ -1,7 +0,0 @@
-document.write('<div class="footmune">');
-document.write('<span onclick=\"pvStatistic(1);window.myAndroidJs.goToMenuPage(1);\"><img src=\"./img/home_web.png\"><p>首页</p></span>');
-document.write('<span onclick=\"window.myAndroidJs.goToMenuPage(2);\"><img src=\"./img/category_web.png\"><p>分类</p></span>');
-document.write('<span onclick=\"window.myAndroidJs.goToMenuPage(3);\"><img src=\"./img/hot_web.png\"><p>特卖</p></span>');
-document.write('<span onclick=\"window.myAndroidJs.goToMenuPage(4);\"><img src=\"./img/hudong_web.png\"><p>互动</p></span>');
-document.write('<span onclick=\"window.myAndroidJs.goToMenuPage(5);\"><img src=\"./img/more_web.png\"><p>更多</p></span>');
-document.write('<div>');
